42ce5dadf6068da4d823fe5d336fff14af1e5580
Istvan Viczian <viczian.istvan@gmail.com>
2022-10-11 20:34:55 +0200

    References

---
+++ references.py
@@ -0,0 +1,27 @@
+numbers = [1, 2, 3, 4, 5]
+
+another_numbers = numbers
+
+another_numbers[2] = 66
+
+print(numbers)
+print(another_numbers)
+
+numbers[3] = 67
+print(another_numbers)
+
+i = 10
+j = i
+j = 12
+print(i)
+
+
+numbers = [1, 2, 3, 4, 5]
+another_numbers = numbers
+another_numbers = [1, 2, 3]
+print(numbers)
+
+
+numbers = [1, 2, 3]
+numbers = [0, 1, 2]
+
