d6794fe7668245a89ac65ea7987342ebbd3061d4
Istvan Viczian <viczian.istvan@gmail.com>
2022-10-12 11:29:29 +0200

    Kiindulási feladat

---
+++ geometry.py
@@ -0,0 +1,5 @@
+# Hozzatok létre egy olyan Circle nevezetű osztályt,
+# melynek konstruktorban át kell adni egy sugarat!
+# Írjatok egy metódust, ami lekéri a sugár méretét (get)!
+# Írjatok egy másik metódust, ami visszaadja a terültetét!
+# t = r ** 2 * pi
